b3b77af4482d4665121c006e6e800ef673a34ee6
Che Smith <cwsmith@usa.net>
2020-02-19 10:26:23 -0600

    Simple test for a NEO and Spark Max

---
+++ NEOTest/src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -0,0 +1,36 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.JoystickConstants;
+
+public class DriveTrain extends SubsystemBase {
+  /**
+   * Creates a new DriveTrain.
+   */
+
+  CANSparkMax _spark = new CANSparkMax(1, MotorType.kBrushless);
+
+  public DriveTrain() {
+
+  }
+
+  public void teleopDrive(Joystick joystick) {
+    _spark.set(joystick.getRawAxis(JoystickConstants.LEFT_STICK_Y));
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
